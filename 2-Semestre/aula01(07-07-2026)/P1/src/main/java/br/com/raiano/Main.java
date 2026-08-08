80b508af5b26a570b64a9d9b294df840b22c5e4b
Matheus Raiano <123113785+matheusraiano@users.noreply.github.com>
2026-08-07 22:22:21 -0300

    Add files via upload

---
+++ 2-Semestre/aula01(07-07-2026)/P1/src/main/java/br/com/raiano/Main.java
@@ -0,0 +1,13 @@
+package br.com.raiano;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("EXEMPLO PROFESSOR:");
+        pessoas.pessoa1();
+        System.out.println("EXERCÍCIO 01");
+        pessoas.pessoa2();
+    }
+}
---
+++ 2-Semestre/aula01(07-07-2026)/P1/src/main/java/br/com/raiano/Pessoa.java
@@ -0,0 +1,7 @@
+package br.com.raiano;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    double peso;
+}
